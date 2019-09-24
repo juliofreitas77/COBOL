acda454dbcc5293ef7d6a6f9040b7ba3fad5292d
Julio Cesar Silva de Freitas <julio.si.freitas@gmail.com>
2019-09-24 17:43:56 -0300

    Função EVALUATE/THRUE

---
+++ PROGCOB08.cob
@@ -0,0 +1,21 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROGCOB08.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       77 WRK-NOTA1 PIC 9(02) VALUE ZEROS.
+       77 WRK-NOTA2 PIC 9(02) VALUE ZEROS.
+       77 WRK-MEDIA PIC 9(02)V9 VALUE ZEROS.
+       PROCEDURE DIVISION.
+           ACCEPT WRK-NOTA1.
+           ACCEPT WRK-NOTA2.
+               COMPUTE WRK-MEDIA=(WRK-NOTA1 + WRK-NOTA2) /2.
+               DISPLAY 'MEDIA ' WRK-MEDIA.
+               EVALUATE WRK-MEDIA
+               WHEN 6 THRU 10
+                   DISPLAY'APROVADO'
+               WHEN 2 THRU 5
+                   DISPLAY'RECUPARECAO'
+               WHEN OTHER
+                   DISPLAY'REPROVADO'.
+           STOP RUN.
